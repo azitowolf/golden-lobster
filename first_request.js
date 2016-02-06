46aaa682ba8bcb8022a684a73b2f7b233d983148
David Goldberg <goodwordalchemy@gmail.com>
2016-02-06 17:11:27 -0500

    wrote amazon api script very first version

---
+++ first_request.js
@@ -0,0 +1,19 @@
+amazon = require('amazon-product-api');
+
+var client = amazon.createClient({
+	awsId: process.env['AWSAccessKeyId'],
+	awsSecret: process.env['AWSSecretKey'],
+	awsTag: process.env['AmazonAssociatesTag']
+});
+
+client.itemSearch({  
+  director: 'Quentin Tarantino',
+  actor: 'Samuel L. Jackson',
+  searchIndex: 'DVD',
+  audienceRating: 'R',
+  responseGroup: 'ItemAttributes,Offers,Images'
+}).then(function(results){
+  console.log(JSON.stringify(results, null, 4));
+}).catch(function(err){
+  console.log(err);
+});
