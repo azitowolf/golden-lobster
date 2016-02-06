da64b0b3fb66ba96432ca57e2910dcd02cb55c05
Miles Ingram <milesingrams@gmail.com>
2016-02-06 16:22:44 -0500

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('goldenLobsterApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
